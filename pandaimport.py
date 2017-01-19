30a1fbae4c11b3bedf287584f01e2e0c431847e1
kefortney <kefortney@gmail.com>
2017-01-19 10:39:55 -0500

    Addition of iPython notebook, changes to a couple of files

--- pandaimport.py
+++ pandaimport.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as nm
-dataimport = pd.read_csv('~/Desktop/pythonexplore/ProperySalesPrice.csv',header='infer')
+import numpy as np
+yummy = pd.read_csv('~/Desktop/pythonexplore/BurlingtonPropertyDetails.csv')
 
-plt.hist(dataimport['Sale Date'], bins=100)
-dataimport.describe()
+
+shortyummy = yummy[['Bath1','Bath2','NumofBedrooms']]
+# shows the data types
+print(shortyummy)
+
+# pd.to_numeric(bvtdata[Bath1], errors='ignore')
+# print(bvtdata)
+# print(bvtdata['AccountNumber'])
+# plt.hist(shortyummy['NumofBedrooms'], bins=100)
+plt.scatter(shortyummy['NumofBedrooms'],shortyummy['Bath1'])
